3095f869539d788d410eb1e874488aac43c34e0d
progyu <rocket7101@gmail.com>
2020-11-16 00:32:32 +0900

    feat: Change people to movies and  Make Mutations

--- backend/graphql/db.js
+++ backend/graphql/db.js
@@ -1,31 +1,49 @@
-export const people = [
+export let movies = [
   {
-    id: '1',
-    name: 'progyu',
-    age: 30,
-    gender: 'male'
+    id: 0,
+    name: 'StarWars',
+    score: 1
   },
   {
-    id: '2',
-    name: 'gayoung',
-    age: 30,
-    gender: 'female'
+    id: 1,
+    name: 'Avengers - The new one',
+    score: 8
   },
   {
-    id: '3',
-    name: 'musk',
-    age: 56,
-    gender: 'male'
+    id: 2,
+    name: 'The Godfather I',
+    score: 99
   },
   {
-    id: '4',
-    name: 'jobs',
-    age: 70,
-    gender: 'male'
+    id: 3,
+    name: 'Logan',
+    score: 3
   }
 ]
 
+export const getMovies = () => movies
+
 export const getById = id => {
-  const filteredPerson = people.filter(person => person.id === String(id))
-  return filteredPerson[0]
+  const filteredMovies = movies.filter(movie => movie.id === id)
+  return filteredMovies[0]
+}
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id)
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies
+    return true
+  } else {
+    return false
+  }
+}
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  }
+  movies = [...movies, newMovie]
+  return newMovie
 }
