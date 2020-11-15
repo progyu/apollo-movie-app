b1388a4196f32120df32200c74deceb9185f7374
progyu <rocket7101@gmail.com>
2020-11-16 02:21:03 +0900

    update: Wrapping a REST API With GraphQL

--- backend/graphql/db.js
+++ backend/graphql/db.js
@@ -1,49 +1,46 @@
-export let movies = [
-  {
-    id: 0,
-    name: 'StarWars',
-    score: 1
-  },
-  {
-    id: 1,
-    name: 'Avengers - The new one',
-    score: 8
-  },
-  {
-    id: 2,
-    name: 'The Godfather I',
-    score: 99
-  },
-  {
-    id: 3,
-    name: 'Logan',
-    score: 3
-  }
-]
+import axios from 'axios'
 
-export const getMovies = () => movies
+const BASE_URL = 'https://yts.mx/api/v2/'
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`
 
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => movie.id === id)
-  return filteredMovies[0]
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
+    }
+  })
+  return movies
 }
 
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id)
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies
-    return true
-  } else {
-    return false
-  }
+export const getMovie = async id => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id
+    }
+  })
+  return movie
 }
 
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score
-  }
-  movies = [...movies, newMovie]
-  return newMovie
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id
+    }
+  })
+  return movies
 }
